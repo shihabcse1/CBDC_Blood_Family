71f8085fc060514ebd1c509db87afb274efe279a
Shihab <tomdshihab@gmail.com>
2021-08-25 07:42:47 +0600

    Working on V2. Language Change code added.

--- app/src/main/java/com/shihabcse/cbdcbloodfamily/activities/LoginActivity.java
+++ app/src/main/java/com/shihabcse/cbdcbloodfamily/activities/LoginActivity.java
@@ -1,10 +1,15 @@
 package com.shihabcse.cbdcbloodfamily.activities;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
@@ -19,14 +24,14 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.shihabcse.cbdcbloodfamily.R;
 
+import java.util.Locale;
+
 public class LoginActivity extends AppCompatActivity {
 
     private Button buttonCreateAnAccount, buttonSignIn;
     private EditText editTextLoginEmail, editTextLoginPassword;
-
-
+    private SwitchCompat switchCompat;
     private FirebaseAuth mAuth;
-
     private ProgressDialog progressDialog;
 
     @Override
@@ -49,16 +54,38 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-
+        
+        // load language selection
+        loadLocale();
+        // for changing the language of ActionBar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(getResources().getString(R.string.app_name));
+        
         mAuth = FirebaseAuth.getInstance();
-
+        switchCompat = findViewById(R.id.switch_language);
         buttonCreateAnAccount = findViewById(R.id.button_create_new_account);
         buttonSignIn = findViewById(R.id.button_sign_in);
         editTextLoginEmail = findViewById(R.id.editTextText_user_email);
         editTextLoginPassword = findViewById(R.id.editTextText_register_user_password);
 
         progressDialog = new ProgressDialog(this);
+
+        // Select language by switchCompat
+        switchCompat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (switchCompat.isChecked()) {
+
+                    setLocale("bn");
+
+                } else {
+
+                    setLocale("en");
+
+                }
+                recreate();
+            }
+        });
 
         buttonSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,6 +118,24 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(registerActivity);
             }
         });
+    }
+
+    private void loadLocale() {
+        SharedPreferences preferences = getSharedPreferences("settings", Activity.MODE_PRIVATE);
+        String language = preferences.getString("My_Lang", "");
+        setLocale(language);
+    }
+
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration configuration = new Configuration();
+        configuration.locale = locale;
+        getBaseContext().getResources().updateConfiguration(configuration, getBaseContext().getResources().getDisplayMetrics());
+        // save data to shared preference
+        SharedPreferences.Editor editor = getSharedPreferences("settings", MODE_PRIVATE).edit();
+        editor.putString("My_Lang", lang);
+        editor.apply();
     }
 
     private boolean isUserInputValidation(String mail, String password) {
